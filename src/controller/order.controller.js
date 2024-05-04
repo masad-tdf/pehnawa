8bba1739d8b047a85743245b5755b378e3bb9220
masad-tdf <134927728+masad-tdf@users.noreply.github.com>
2024-05-04 11:43:47 +0500

    Update order.controller.js

--- src/controller/order.controller.js
+++ src/controller/order.controller.js
@@ -5,26 +5,25 @@ import createError from "../utils/createError.js";
 export const placeOrder = async (req, res, next) => {
   try {
     // Extract data from the request body
-    const { consumerInfo, productsInfo, deliveryInfo } = req.body;
+    const { billingDetails, OrderSummary} = req.body;
 
     // Calculate total amount, discount, and payable amount for each product
-    for (const product of productsInfo) {
-      const { price, discount } = await Product.findById(product.productId); // Get product price from database
-      const totalAmount = price * product.qty;
-      const payableAmount = totalAmount - (discount * totalAmount) / 100;
+    // for (const product of productsInfo) {
+    //   const { price, discount } = await Product.findById(product.productId); // Get product price from database
+    //   const totalAmount = price * product.qty;
+    //   const payableAmount = totalAmount - (discount * totalAmount) / 100;
 
-      product.totalAmount = totalAmount;
-      product.discount = discount;
-      product.payableAmount = payableAmount;
-      product.productId = product.productId.toString(); // Convert productId to string
-      delete product._id; // Remove the _id field
-    }
+    //   product.totalAmount = totalAmount;
+    //   product.discount = discount;
+    //   product.payableAmount = payableAmount;
+    //   product.productId = product.productId.toString(); // Convert productId to string
+    //   delete product._id; // Remove the _id field
+    // }
 
     // Create a new order instance using the Order model
     const newOrder = new Order({
-      consumerInfo,
-      productsInfo,
-      deliveryInfo,
+      billingDetails,
+      OrderSummary,
     });
 
     // Save the order to the database
@@ -56,11 +55,13 @@ export const updateOrder = async (req, res, next) => {
     next(err);
   }
 };
+
 export const getAllOrders = async (req, res, next) => {
   try {
     //{deliveryInfo.deliveryStatus : "Scheduled"}
-    const { status } = req.params;
-    const orders = await Order.find({ 'deliveryInfo.deliveryStatus': status });
+    //const { status } = req.params;
+    //const orders = await Order.find({ 'deliveryInfo.deliveryStatus': status });
+    const orders = await Order.find();
     if (!orders) return next(createError(204, "Order not placed yet"));
       res.status(200).send(orders);
   } catch (err) {
